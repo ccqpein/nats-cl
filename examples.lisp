6f358986932d0880abdf448cb3c39c35423eb763
ccQpein <ccqpein@protonmail.com>
2020-04-18 13:35:17 -0400

    more example

--- examples.lisp
+++ examples.lisp
@@ -34,3 +34,14 @@
             5
             "hello"
             ))
+
+
+;;; write you own message handler
+(let ((sokt (connect-nats-server "127.0.0.1"))
+      info)
+  (multiple-value-setq (sokt info) (post-connection sokt))
+  ;; with-nats-stream macro will read message in stream one by one
+  ;; then binding message with data which used in body
+  ;; sokt will close when with-nats-stream macro finish
+  (with-nats-stream (sokt data)
+    (format t "~a~%" data)))
