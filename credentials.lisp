a777fcf777e22fa08ac642efec814a6117b1a19e
ccQpein <ccqpein@protonmail.com>
2020-04-29 16:15:18 -0400

    a bit

--- credentials.lisp
+++ credentials.lisp
@@ -2,3 +2,12 @@
   (:use #:CL)) 
 
 (in-package #:nats-cred)
+
+(defun read-creds-file (filepath)
+  "read creds file and return JWT and pk"
+  )
+
+(defun sig-nonce (nonce)
+  "return signature generated from nonce"
+  )
+
