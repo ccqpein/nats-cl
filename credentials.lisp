60b2658f468f60c19c50ebebf167f2b9b92d313b
ccQpein <ccqpein@protonmail.com>
2020-04-30 19:29:22 -0400

    finish cred read function

--- credentials.lisp
+++ credentials.lisp
@@ -4,8 +4,29 @@
 (in-package #:nats-cred)
 
 (defun read-creds-file (filepath)
-  "read creds file and return JWT and pk"
-  )
+  "read creds file and return JWT and nkey"
+  (with-open-file (s filepath)
+    (loop
+      with flag and jwt and nkey
+      for line = (read-line s nil)
+      while line
+      do (cond ((string= line "-----BEGIN NATS USER JWT-----")
+                (setf flag 'jwt)) ;; read jwt
+               
+               ((or (string= line "------END NATS USER JWT------")
+                    (string= line "------END USER NKEY SEED------"))
+                (setf flag nil))
+
+               ((string= line "-----BEGIN USER NKEY SEED-----")
+                (setf flag 'nkey))
+
+               (t
+                (case flag
+                  (jwt (setf jwt line))
+                  (nkey (setf nkey line))
+                  (otherwise nil))))
+      finally (return (values jwt nkey))
+     )))
 
 (defun sig-nonce (nonce)
   "return signature generated from nonce"
